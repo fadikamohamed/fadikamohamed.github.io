bc240df6bf9a3a63c6a24f3707cf5a2430b14f87
fadikamohamed <fadika.mohamed02@gmail.com>
2018-09-02 12:23:52 +0200

    Responsive terminé et navbar terminé

--- assets/js/fonction.js
+++ assets/js/fonction.js
@@ -1,54 +1,50 @@
 $(window).scroll(function(e){
-    parallax();
+  parallax();
 });
 function parallax(){
 
-    var scrolledTop = $(window).scrollTop();
-     $('#pageBackground').css('top', -(scrolledTop * 0.2) + 'px');
-	   $('#contentCV').css('top', -(scrolledTop * 0.3) + 'px');
-     $('.parts').css('top', -(scrolledTop * 0.5) + 'px');
+  var scrolledTop = $(window).scrollTop();
+  $('#pageBackground').css('top', -(scrolledTop * 0.2) + 'px');
+  $('#contentCV').css('top', -(scrolledTop * 0.3) + 'px');
+  $('.parts').css('top', -(scrolledTop * 0.5) + 'px');
 }
-
 $(function() {
-$('#presentation').waypoint(function(direction) {
-  if (direction == 'down') {
-    $('#pageBackground').removeClass().addClass('pageBackground1');
-  }
-});
-$('#presentation').waypoint(function(direction) {
-  if (direction == 'up') {
-    $('#pageBackground').removeClass().addClass('pageBackground');
-  }
-});
-
-
-//Second
-$('#f2').waypoint(function(direction) {
-  if (direction == 'down') {
-    $('#pageBackground').removeClass().addClass('pageBackground2');
-  }
-});
-$('#f2').waypoint(function(direction) {
-  if (direction == 'up') {
-    $('#pageBackground').removeClass().addClass('pageBackground1');
-  }
-});
-
-//Third
-$('#f3').waypoint(function(direction) {
-  if (direction === 'down') {
-    $('#pageBackground').addClass('pageBackground3');
-  }else if (direction === 'up') {
-    $('#pageBackground').removeClass().addClass('pageBackground2');
-  } else {
-  }
-});
-$('#ancre').waypoint(function(direction) {
-  if (direction === 'down') {
-    $('#pageBackground').addClass('pageBackground4');
-  }else if (direction === 'up') {
-    $('#pageBackground').removeClass().addClass('pageBackground3');
-  } else {
-  }
-});
+  $('#presentation').waypoint(function(direction) {
+    if (direction == 'down') {
+      $('#pageBackground').removeClass().addClass('pageBackground1');
+    }
+  });
+  $('#presentation').waypoint(function(direction) {
+    if (direction == 'up') {
+      $('#pageBackground').removeClass().addClass('pageBackground');
+    }
+  });
+  //Second
+  $('#f2').waypoint(function(direction) {
+    if (direction == 'down') {
+      $('#pageBackground').removeClass().addClass('pageBackground2');
+    }
+  });
+  $('#f2').waypoint(function(direction) {
+    if (direction == 'up') {
+      $('#pageBackground').removeClass().addClass('pageBackground1');
+    }
+  });
+  //Third
+  $('#f3').waypoint(function(direction) {
+    if (direction === 'down') {
+      $('#pageBackground').addClass('pageBackground3');
+    }else if (direction === 'up') {
+      $('#pageBackground').removeClass().addClass('pageBackground2');
+    } else {
+    }
+  });
+  $('#pointOfInterest').waypoint(function(direction) {
+    if (direction === 'down') {
+      $('#pageBackground').addClass('pageBackground4');
+    }else if (direction === 'up') {
+      $('#pageBackground').removeClass().addClass('pageBackground3');
+    } else {
+    }
+  });
 });
